5fc7d8b8093ef8fec839742df74c5b85f80bfa63
iman <iman@bitmascot.com>
2017-05-28 14:40:21 +0600

    crud

--- document/crud.php
+++ document/crud.php
@@ -1,3 +1,13 @@
 Resource controller:
 
 php artisan make:controller ProductController --resource
+
+https://laravel.com/docs/5.4/migrations
+php artisan make:migration create_products_table
+
+php artisan make:migration create_products_table --create=products
+
+/*
+composer dump-autoload
+php artisan migrate:rollback
+*
